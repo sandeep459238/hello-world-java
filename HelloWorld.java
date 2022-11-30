f1b6d49ae7c1772a46091355721924336de7a85e
Sandeep Singh <93819717+sandeep459238@users.noreply.github.com>
2022-11-30 14:55:41 +0530

    Update HelloWorld.java
    remove lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -36,12 +36,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Good Morning!----->");
     System.out.println("Good Morning!----->");
-    System.out.println("Good Morning!----->");
-    System.out.println("Good Morning!----->");
-    System.out.println("Good Morning!----->");
-    System.out.println("Good Morning!----->");
-    System.out.println("Good Morning!----->");
-    System.out.println("Good Morning!----->");
-    System.out.println("Good Morning!----->");
+
   }
 }
