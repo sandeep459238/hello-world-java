a7748d41c0a47d0c1b66cbc8190fbc01f2e78ce4
root <root@ip-172-31-86-28.ec2.internal>
2022-11-30 08:58:52 +0000

    adding new lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -34,6 +34,14 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, India");
+    System.out.println("Good Morning!----->");
+    System.out.println("Good Morning!----->");
+    System.out.println("Good Morning!----->");
+    System.out.println("Good Morning!----->");
+    System.out.println("Good Morning!----->");
+    System.out.println("Good Morning!----->");
+    System.out.println("Good Morning!----->");
+    System.out.println("Good Morning!----->");
+    System.out.println("Good Morning!----->");
   }
 }
